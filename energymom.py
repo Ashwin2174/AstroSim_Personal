52aa8196729fdbf450147f5cf13aefdc16d67319
Ashwin2174 <ashwinsubramanian.murugan@gmail.com>
2024-07-28 15:22:19 +0530

    added readme

--- energymom.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot  as plt
-
-e,px,py,pz = np.loadtxt("foo1.csv", unpack=True, delimiter=",")
-
-plt.title("Energy of the System")
-plt.plot([i for i in range(len(e))],e)
-plt.show()
